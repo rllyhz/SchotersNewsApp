b1de88c4150c2327c0145547f7b494340576c458
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-24 22:24:58 +0700

    Added providers for RemoteDataSource, Repository and DispatcherProvider

--- app/src/main/java/id/rllyhz/schotersnewsapp/utils/Constants.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/utils/Constants.kt
@@ -1,12 +1,17 @@
 package id.rllyhz.schotersnewsapp.utils
 
 import id.rllyhz.schotersnewsapp.BuildConfig
+import id.rllyhz.schotersnewsapp.data.source.NewsRepository
+import id.rllyhz.schotersnewsapp.data.source.remote.RemoteDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 object Constants {
     const val baseUrl = "https://newsapi.org/"
     const val apiKey = BuildConfig.API_KEY
+
+    private fun getRemoteDataSource() = RemoteDataSource.getInstance()
+    fun getRepository() = NewsRepository.getInstance(getRemoteDataSource())
 
     val dispatchersProvider = object : DispatcherProvider {
         override val main: CoroutineDispatcher
