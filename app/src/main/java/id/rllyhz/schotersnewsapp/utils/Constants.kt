9260c456a7151657fa318092bfa689faf40a97bb
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-24 19:23:54 +0700

    Created constants file for storing constant values

--- app/src/main/java/id/rllyhz/schotersnewsapp/utils/Constants.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/utils/Constants.kt
@@ -1,4 +1,8 @@
 package id.rllyhz.schotersnewsapp.utils
 
+import id.rllyhz.schotersnewsapp.BuildConfig
+
 object Constants {
+    const val baseUrl = "https://newsapi.org/"
+    const val apiKey = BuildConfig.API_KEY
 }
