b93db27515263f40956825a209af96661db28510
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-24 20:57:16 +0700

    Created dispatcher interface for providing dispatcher's intance

--- app/src/main/java/id/rllyhz/schotersnewsapp/utils/Constants.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/utils/Constants.kt
@@ -1,8 +1,21 @@
 package id.rllyhz.schotersnewsapp.utils
 
 import id.rllyhz.schotersnewsapp.BuildConfig
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
 object Constants {
     const val baseUrl = "https://newsapi.org/"
     const val apiKey = BuildConfig.API_KEY
+
+    val dispatchersProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
 }
