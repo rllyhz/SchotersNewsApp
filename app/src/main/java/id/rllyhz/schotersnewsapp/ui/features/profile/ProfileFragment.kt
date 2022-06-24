47d110630df2d28d43c37efa2e7137b0bc6f4c47
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-24 18:34:11 +0700

    Created fragment implementation of home, search and profile screens

--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/profile/ProfileFragment.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/profile/ProfileFragment.kt
@@ -1,6 +1,27 @@
 package id.rllyhz.schotersnewsapp.ui.features.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import id.rllyhz.schotersnewsapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
