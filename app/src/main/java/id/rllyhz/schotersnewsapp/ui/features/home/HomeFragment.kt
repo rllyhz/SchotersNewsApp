6e18b71b240db79c1009eb7dbd11f78768b8f3fb
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-24 23:48:23 +0700

    Updated Home Screen | Implementing shimmer effect while loading

--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/home/HomeFragment.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/home/HomeFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import id.rllyhz.schotersnewsapp.databinding.FragmentHomeBinding
+import id.rllyhz.schotersnewsapp.utils.Constants
+import id.rllyhz.schotersnewsapp.utils.hide
+import id.rllyhz.schotersnewsapp.utils.show
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private var viewModel: HomeViewModel? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +26,52 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(
+            this,
+            HomeViewModel.Factory(Constants.getRepository(), Constants.dispatchersProvider)
+        )[HomeViewModel::class.java]
+
+        viewModel?.let { vm ->
+            vm.news.observe(viewLifecycleOwner) { news ->
+                if (!news.isNullOrEmpty()) {
+                    binding.apply {
+                        homeRv.show()
+                        homeShimmerLayout.hide()
+                        homeShimmerLayout.stopShimmer()
+                    }
+                }
+            }
+
+            vm.shouldLoading.observe(viewLifecycleOwner) { loading ->
+                if (loading) {
+                    binding.apply {
+                        homeRv.hide()
+                        homeShimmerLayout.show()
+                        homeShimmerLayout.startShimmer()
+                    }
+                }
+            }
+
+            vm.isError.observe(viewLifecycleOwner) { errorOccurred ->
+                if (errorOccurred) {
+                    binding.apply {
+                        homeRv.hide()
+                        homeShimmerLayout.hide()
+                        homeShimmerLayout.stopShimmer()
+                    }
+                }
+            }
+
+            vm.getTrendingNews()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        viewModel = null
     }
 }
