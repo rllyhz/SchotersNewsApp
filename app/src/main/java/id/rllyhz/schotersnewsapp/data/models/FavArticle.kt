020cd65aa3a682f659b38f33daf0dbf2ad39c727
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-26 02:59:34 +0700

    Created model entity of article for table in database | creating FavArticle.kt file

---
+++ app/src/main/java/id/rllyhz/schotersnewsapp/data/models/FavArticle.kt
@@ -0,0 +1,23 @@
+package id.rllyhz.schotersnewsapp.data.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(
+    tableName = "articles"
+)
+data class FavArticle(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    val title: String,
+    val url: String,
+    @ColumnInfo(name = "url_to_image")
+    val urlToImage: String,
+    val author: String,
+    val content: String,
+    val description: String,
+    @ColumnInfo(name = "published_at")
+    val publishedAt: String,
+) : Serializable
---
+++ app/src/main/java/id/rllyhz/schotersnewsapp/db/NewsDatabase.kt
@@ -0,0 +1,4 @@
+package id.rllyhz.schotersnewsapp.db
+
+class NewsDatabase {
+}
