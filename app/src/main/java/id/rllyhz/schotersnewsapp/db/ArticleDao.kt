72d8c36bb278c174bf43d2b3e9b4fba87d794e88
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-26 03:01:18 +0700

    Created Database declaration and DAO file | creating NewsDatabase.kt and ArticleDao.kt files

--- app/src/main/java/id/rllyhz/schotersnewsapp/db/ArticleDao.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/db/ArticleDao.kt
@@ -1,4 +1,17 @@
 package id.rllyhz.schotersnewsapp.db
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import id.rllyhz.schotersnewsapp.data.models.FavArticle
+
+@Dao
 interface ArticleDao {
+    @Query("SELECT * FROM articles")
+    fun getAllNews(): LiveData<List<FavArticle>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdate(news: FavArticle): Long
+
+    @Delete
+    suspend fun deleteNews(news: FavArticle)
 }
--- app/src/main/java/id/rllyhz/schotersnewsapp/db/NewsDatabase.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/db/NewsDatabase.kt
@@ -1,4 +1,30 @@
 package id.rllyhz.schotersnewsapp.db
 
-class NewsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import id.rllyhz.schotersnewsapp.data.models.FavArticle
+
+@Database(
+    entities = [FavArticle::class],
+    version = 1
+)
+abstract class NewsDatabase : RoomDatabase() {
+    abstract fun getDao(): ArticleDao
+
+    companion object {
+        @Volatile
+        private var instance: NewsDatabase? = null
+
+        fun getInstance(context: Context): NewsDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    NewsDatabase::class.java,
+                    "news.db"
+                )
+                    .build()
+            }
+    }
 }
