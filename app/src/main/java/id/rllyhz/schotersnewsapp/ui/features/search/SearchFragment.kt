1039e3612b682e0df5740681215f9755f9fe7983
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-25 20:45:52 +0700

    Fixed up searching functionality of Search Screen

--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/search/SearchFragment.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/search/SearchFragment.kt
@@ -1,6 +1,7 @@
 package id.rllyhz.schotersnewsapp.ui.features.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +9,30 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.rllyhz.schotersnewsapp.data.models.Article
 import id.rllyhz.schotersnewsapp.databinding.FragmentSearchBinding
+import id.rllyhz.schotersnewsapp.ui.adapters.ArticleListAdapter
+import id.rllyhz.schotersnewsapp.utils.Constants
+import id.rllyhz.schotersnewsapp.utils.hide
+import id.rllyhz.schotersnewsapp.utils.show
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), ArticleListAdapter.ItemClickCallback {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
+    private var articleListAdapter: ArticleListAdapter? = null
     private var editorListener: TextView.OnEditorActionListener? = null
+    private var viewModel: SearchViewModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        articleListAdapter = ArticleListAdapter()
+        articleListAdapter?.setOnItemClickListener(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,29 +43,68 @@ class SearchFragment : Fragment() {
         return binding.root
     }
 
-    override fun onResume() {
-        super.onResume()
-        binding.searchLoadingAnimView.playAnimation()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        binding.searchLoadingAnimView.pauseAnimation()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(
+            this,
+            SearchViewModel.Factory(Constants.getRepository(), Constants.dispatchersProvider)
+        )[SearchViewModel::class.java]
+
         createEditorListener()
 
-        binding.apply {
-            searchEtSearch.setOnEditorActionListener(editorListener)
+        viewModel?.let { vm ->
+            binding.apply {
+                searchEtSearch.setOnEditorActionListener(editorListener)
+                searchLoadingAnimView.hide()
+
+                searchRv.adapter = articleListAdapter
+                searchRv.layoutManager = LinearLayoutManager(requireContext())
+                searchRv.hide()
+            }
+
+            vm.searchNews.observe(viewLifecycleOwner) { news ->
+                if (!news.isNullOrEmpty()) {
+                    binding.apply {
+                        articleListAdapter?.submitList(news)
+
+                        searchRv.show()
+                    }
+                } else {
+                    // data not found
+                }
+
+                binding.apply {
+                    searchLoadingAnimView.hide()
+                    searchLoadingAnimView.cancelAnimation()
+                }
+            }
+
+            vm.shouldLoading.observe(viewLifecycleOwner) { loading ->
+                if (loading) {
+                    binding.apply {
+                        searchRv.hide()
+                        searchLoadingAnimView.show()
+                        searchLoadingAnimView.playAnimation()
+                    }
+                }
+            }
+
+            vm.isError.observe(viewLifecycleOwner) { errorOccurred ->
+                if (errorOccurred) {
+                    binding.apply {
+                        searchRv.hide()
+                        searchLoadingAnimView.hide()
+                        searchLoadingAnimView.cancelAnimation()
+                    }
+                }
+            }
         }
     }
 
     private fun searchNews() {
-        val text = binding.searchEtSearch.text.toString()
-        Toast.makeText(requireContext(), "Searching $text...", Toast.LENGTH_SHORT).show()
+        val query = binding.searchEtSearch.text.toString()
+        viewModel?.searchNews(query)
     }
 
     private fun createEditorListener() {
@@ -59,10 +116,21 @@ class SearchFragment : Fragment() {
         }
     }
 
+    override fun onClick(article: Article) {
+        Log.d("myapp", "Searched Item clicked!")
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         binding.searchEtSearch.setOnEditorActionListener(null)
+        binding.searchRv.adapter = null
         editorListener = null
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        articleListAdapter?.removeItemClickListener()
+        articleListAdapter = null
     }
 }
--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/search/SearchViewModel.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/search/SearchViewModel.kt
@@ -1,4 +1,52 @@
 package id.rllyhz.schotersnewsapp.ui.features.search
 
-class SearchViewModel {
+import androidx.lifecycle.*
+import id.rllyhz.schotersnewsapp.data.models.Article
+import id.rllyhz.schotersnewsapp.data.source.NewsRepository
+import id.rllyhz.schotersnewsapp.ui.features.home.HomeViewModel
+import id.rllyhz.schotersnewsapp.utils.DispatcherProvider
+import id.rllyhz.schotersnewsapp.utils.Resource
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class SearchViewModel(
+    private val repository: NewsRepository,
+    private val dispatchers: DispatcherProvider
+) : ViewModel() {
+    private val _searchResult: MutableLiveData<List<Article>?> = MutableLiveData(null)
+    val searchNews: LiveData<List<Article>?> = _searchResult
+
+    val shouldLoading = MutableLiveData(false)
+    val isError = MutableLiveData(false)
+
+    fun searchNews(queryString: String) = viewModelScope.launch(dispatchers.io) {
+        val result = repository.searchNews(queryString)
+
+        when (result.value) {
+            is Resource.Loading -> withContext(dispatchers.main) {
+                shouldLoading.value = true
+                isError.value = false
+                _searchResult.value = result.value?.data
+            }
+            is Resource.Error -> withContext(dispatchers.main) {
+                shouldLoading.value = false
+                isError.value = true
+                _searchResult.value = result.value?.data
+            }
+            is Resource.Success -> withContext(dispatchers.main) {
+                shouldLoading.value = false
+                isError.value = false
+                _searchResult.value = result.value!!.data
+            }
+            else -> Unit
+        }
+    }
+
+    class Factory(
+        private val repository: NewsRepository,
+        private val dispatchers: DispatcherProvider
+    ) : ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            SearchViewModel(repository, dispatchers) as T
+    }
 }
