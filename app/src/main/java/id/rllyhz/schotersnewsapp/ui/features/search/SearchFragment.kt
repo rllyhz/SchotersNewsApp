fb768e6530243d38e6738f54b1cb81b3aa4f0e90
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-25 19:44:35 +0700

    Added Searching UI on Search Screen | Implementing EditText as Searching UI

--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/search/SearchFragment.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/search/SearchFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import id.rllyhz.schotersnewsapp.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    private var editorListener: TextView.OnEditorActionListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,16 +27,42 @@ class SearchFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        binding.loadingAnimView.playAnimation()
+        binding.searchLoadingAnimView.playAnimation()
     }
 
     override fun onPause() {
         super.onPause()
-        binding.loadingAnimView.pauseAnimation()
+        binding.searchLoadingAnimView.pauseAnimation()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createEditorListener()
+
+        binding.apply {
+            searchEtSearch.setOnEditorActionListener(editorListener)
+        }
+    }
+
+    private fun searchNews() {
+        val text = binding.searchEtSearch.text.toString()
+        Toast.makeText(requireContext(), "Searching $text...", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createEditorListener() {
+        editorListener = TextView.OnEditorActionListener { _, actionId, _ ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> searchNews()
+            }
+            true
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding.searchEtSearch.setOnEditorActionListener(null)
+        editorListener = null
         _binding = null
     }
 }
