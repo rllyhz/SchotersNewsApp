df25bafe74c51bae2dff988568d75e61da96e4b0
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-26 03:08:09 +0700

    Updated data source and repository | implementing local data source to data source and repository

--- app/src/main/java/id/rllyhz/schotersnewsapp/data/source/NewsDataSource.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/data/source/NewsDataSource.kt
@@ -2,9 +2,13 @@ package id.rllyhz.schotersnewsapp.data.source
 
 import androidx.lifecycle.LiveData
 import id.rllyhz.schotersnewsapp.data.models.Article
+import id.rllyhz.schotersnewsapp.data.models.FavArticle
 import id.rllyhz.schotersnewsapp.utils.Resource
 
 interface NewsDataSource {
     suspend fun getTrendingNews(): LiveData<Resource<List<Article>>>
     suspend fun searchNews(query: String): LiveData<Resource<List<Article>>>
+    fun getAllFavNews(): LiveData<List<FavArticle>>
+    suspend fun insertOrUpdateFavNews(favNews: FavArticle): Long
+    suspend fun deleteFavNews(favNews: FavArticle)
 }
--- app/src/main/java/id/rllyhz/schotersnewsapp/data/source/NewsRepository.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/data/source/NewsRepository.kt
@@ -2,11 +2,14 @@ package id.rllyhz.schotersnewsapp.data.source
 
 import androidx.lifecycle.LiveData
 import id.rllyhz.schotersnewsapp.data.models.Article
+import id.rllyhz.schotersnewsapp.data.models.FavArticle
+import id.rllyhz.schotersnewsapp.data.source.local.LocalDataSource
 import id.rllyhz.schotersnewsapp.data.source.remote.RemoteDataSource
 import id.rllyhz.schotersnewsapp.utils.Resource
 
 class NewsRepository(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource
 ) : NewsDataSource {
     override suspend fun getTrendingNews(): LiveData<Resource<List<Article>>> =
         remoteDataSource.getTrendingNews()
@@ -14,14 +17,24 @@ class NewsRepository(
     override suspend fun searchNews(query: String): LiveData<Resource<List<Article>>> =
         remoteDataSource.searchNews(query)
 
+    override fun getAllFavNews(): LiveData<List<FavArticle>> =
+        localDataSource.getAllFavNews()
+
+    override suspend fun insertOrUpdateFavNews(favNews: FavArticle): Long =
+        localDataSource.insertOrUpdateFavNews(favNews)
+
+    override suspend fun deleteFavNews(favNews: FavArticle) =
+        localDataSource.deleteFavNews(favNews)
+
     companion object {
         private var instance: NewsRepository? = null
 
         fun getInstance(
-            remoteDataSource: RemoteDataSource
+            remoteDataSource: RemoteDataSource,
+            localDataSource: LocalDataSource
         ): NewsRepository =
             (instance ?: synchronized(this) {
-                instance ?: NewsRepository(remoteDataSource)
+                instance ?: NewsRepository(remoteDataSource, localDataSource)
             })
     }
 }
