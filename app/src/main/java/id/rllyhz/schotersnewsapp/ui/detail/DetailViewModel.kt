62c277d6d5e89465920207a2f0d375b68797d94b
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-26 02:25:47 +0700

    Created DetailViewModel.kt

--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/detail/DetailViewModel.kt
@@ -1,4 +1,22 @@
 package id.rllyhz.schotersnewsapp.ui.detail
 
-class DetailViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import id.rllyhz.schotersnewsapp.data.source.NewsRepository
+
+class DetailViewModel(
+    private val repository: NewsRepository
+) : ViewModel() {
+    var isFavorite = MutableLiveData(false)
+
+    fun addOrDeleteFromFav() {
+        val favorite = isFavorite.value!!
+        isFavorite.value = !favorite
+    }
+
+    class Factory(private val repository: NewsRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            DetailViewModel(repository) as T
+    }
 }
