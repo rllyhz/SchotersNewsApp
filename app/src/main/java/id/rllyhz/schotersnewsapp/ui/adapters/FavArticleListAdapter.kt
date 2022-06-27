3c22bf6feb4452b8ecb82e5f0a764d9054b74dc5
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-06-27 16:38:53 +0700

    Set up Recyclerview on Favorites screen

---
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/adapters/FavArticleListAdapter.kt
@@ -0,0 +1,75 @@
+package id.rllyhz.schotersnewsapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import id.rllyhz.schotersnewsapp.R
+import id.rllyhz.schotersnewsapp.data.models.FavArticle
+import id.rllyhz.schotersnewsapp.databinding.ItemArticleBinding
+import id.rllyhz.schotersnewsapp.utils.formalizeDate
+
+class FavArticleListAdapter :
+    ListAdapter<FavArticle, FavArticleListAdapter.FavArticleListViewHolder>(FavArticleComparator()) {
+    private var callback: FavArticleListAdapter.ItemClickCallback? = null
+
+    fun setOnItemClickListener(listener: FavArticleListAdapter.ItemClickCallback) {
+        callback = listener
+    }
+
+    // removing listener to avoid memory leaks
+    fun removeItemClickListener() {
+        callback = null
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavArticleListViewHolder {
+        val binding = ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FavArticleListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FavArticleListViewHolder, position: Int) {
+        val currentItem = getItem(position)
+
+        if (currentItem != null)
+            holder.bind(currentItem)
+    }
+
+    inner class FavArticleListViewHolder(
+        private val binding: ItemArticleBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(favArticle: FavArticle) {
+            binding.apply {
+
+                Glide.with(itemView)
+                    .asBitmap()
+                    .centerCrop()
+                    .load(favArticle.urlToImage)
+                    .placeholder(R.drawable.bg_placeholder_article_cover)
+                    .into(itemArticleCover)
+
+                itemArticleTitle.text = favArticle.title
+                itemArticlePublishedAt.text = formalizeDate(favArticle.publishedAt)
+
+                itemView.setOnClickListener { //callback?.onClick(favArticle)
+                }
+            }
+        }
+    }
+
+    // callback for handling itemClick event
+    interface ItemClickCallback {
+        fun onClick(favArticle: FavArticle)
+    }
+
+    // Diffutil for comparison
+    class FavArticleComparator : DiffUtil.ItemCallback<FavArticle>() {
+        override fun areItemsTheSame(oldItem: FavArticle, newItem: FavArticle): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: FavArticle, newItem: FavArticle): Boolean =
+            oldItem == newItem
+    }
+}
--- app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/favorites/FavoritesFragment.kt
+++ app/src/main/java/id/rllyhz/schotersnewsapp/ui/features/favorites/FavoritesFragment.kt
@@ -1,17 +1,31 @@
 package id.rllyhz.schotersnewsapp.ui.features.favorites
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import id.rllyhz.schotersnewsapp.data.models.FavArticle
 import id.rllyhz.schotersnewsapp.databinding.FragmentFavoritesBinding
+import id.rllyhz.schotersnewsapp.ui.adapters.FavArticleListAdapter
+import id.rllyhz.schotersnewsapp.ui.detail.DetailActivity
+import id.rllyhz.schotersnewsapp.utils.asModel
 import id.rllyhz.schotersnewsapp.utils.hide
 
-class FavoritesFragment : Fragment() {
+class FavoritesFragment : Fragment(), FavArticleListAdapter.ItemClickCallback {
     private var _binding: FragmentFavoritesBinding? = null
     private val binding get() = _binding!!
+
+    private var favArticleListAdapter: FavArticleListAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        favArticleListAdapter = FavArticleListAdapter()
+        favArticleListAdapter?.setOnItemClickListener(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,8 +47,21 @@ class FavoritesFragment : Fragment() {
         }
     }
 
+    override fun onClick(favArticle: FavArticle) {
+        Intent(requireContext().applicationContext, DetailActivity::class.java).run {
+            putExtra(DetailActivity.ARTICLE_KEY, favArticle.asModel())
+            startActivity(this)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        favArticleListAdapter?.removeItemClickListener()
+        favArticleListAdapter = null
     }
 }
